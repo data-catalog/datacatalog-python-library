e8e2d9dacbd4aaa8e710dfe9ff2e64d227bbe2a0
Cseke Kata <katacseke@gmail.com>
2020-11-03 13:31:57 +0200

    Api generated with user and asset models.

---
+++ data_catalog/client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from data_catalog.client.api.asset_api import AssetApi
+from data_catalog.client.api.user_api import UserApi
