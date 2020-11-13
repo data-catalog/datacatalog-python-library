0a83e5c3e2e338c17002a4931ac11ef44a49536d
Cseke Kata <katacseke@gmail.com>
2020-11-13 18:12:29 +0200

    Tests for asset location written

--- data_catalog/assets/location.py
+++ data_catalog/assets/location.py
@@ -6,6 +6,9 @@ class Location (ClientLocation):
         super().__init__(type=type, parameters=parameters)
 
     def get_parameter(self, key):
+        if self.parameters is None:
+            return None
+
         values = [parameter.value for parameter in self.parameters if parameter.key == key]
 
         return values[0] if len(values) > 0 else None
