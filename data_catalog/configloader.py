aed2044c09b6d1c2c74b83953bdad93e1e940f4d
DESKTOP-QC9DAKK\katac <katacseke@gmail.com>
2021-01-04 18:00:53 +0200

    Separated configuration for the services.

--- data_catalog/configloader.py
+++ data_catalog/configloader.py
@@ -4,11 +4,26 @@ from data_catalog.client.asset import Configuration
 import os
 
 
-def load():
+def _load_config(config_name: str):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(root_dir, 'config.yaml')
 
     config = ConfigLoader()
     config.update_from_yaml_file(config_path)
 
-    return Configuration(host=config['host'])
+    final_config = Configuration(host=config[config_name]['host'])
+    final_config.client_side_validation = config.get('client_side_validation')
+
+    return final_config
+
+
+def load_user_service_config():
+    return _load_config('user_service')
+
+
+def load_asset_service_config():
+    return _load_config('asset_service')
+
+
+def load_versioning_service_config():
+    return _load_config('versioning_service')
