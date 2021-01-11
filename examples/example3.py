02e772d64785d7c63ff4412f54327770c2406a68
Tumó Szilárd <tumoszilard@gmail.com>
2021-01-11 22:47:39 +0200

    Fixed flake8 error.

--- examples/example3.py
+++ examples/example3.py
@@ -10,11 +10,11 @@ sas_token = "?sp=racwdl&st=2020-12-02T12:36:52Z&se=2020-12-20T12:36:00Z&sv=2019-
 container = ContainerClient(account_url="https://datacatalogblob.blob.core.windows.net", container_name="container", credential=sas_token)
 
 blob_asset = asset.Asset('222', format='csv', location=asset.Location('azureblob', parameters=[
-        Parameter('accountUrl', 'https://datacatalogblob.blob.core.windows.net'),
-        Parameter('containerName', 'container'),
-        Parameter('sasToken', 'Q0cS0Cj4LyWy/unaxqD4BWp2niZ7WPzHC8ABEQWBnQ24qFENIBStCtk68T+QZaFxmS5pUqSd+ia8hH2fyYV1PQ=='),
-        Parameter('expiryTime', '2020-12-20T12:36:00Z')
-    ]))
+    Parameter('accountUrl', 'https://datacatalogblob.blob.core.windows.net'),
+    Parameter('containerName', 'container'),
+    Parameter('sasToken', 'Q0cS0Cj4LyWy/unaxqD4BWp2niZ7WPzHC8ABEQWBnQ24qFENIBStCtk68T+QZaFxmS5pUqSd+ia8hH2fyYV1PQ=='),
+    Parameter('expiryTime', '2020-12-20T12:36:00Z')
+]))
 
 blob = container.get_blob_client('nested/more nested/more nested user.csv')
 print(blob.get_blob_properties())
